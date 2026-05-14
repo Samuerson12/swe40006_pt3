76faaa00a354e097c4aa18a07e368fe079ccdff1
Samuerson12 <113032792+Samuerson12@users.noreply.github.com>
2026-05-14 02:05:16 +0000

    Test auto-deploy

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
       <body style="font-family:Arial; max-width:600px; margin:80px auto; text-align:center">
         <h1 style="color:#2e74b5">SWE40006 Portfolio Task 3</h1>
         <p>Deployed by Samuel Chang via Render</p>
-        <p>Flask Web Service ✅</p>
+        <p>Auto-Deploy Test ✅</p>
       </body>
     </html>
     '''
