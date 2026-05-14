d17b0c67a883ff55d269dc64494a0e1abc0cdae4
Samuerson12 <113032792+Samuerson12@users.noreply.github.com>
2026-05-14 02:03:28 +0000

    Add Flask app for Credit task

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+    <html>
+      <head><title>SWE40006 Task 3 - Credit</title></head>
+      <body style="font-family:Arial; max-width:600px; margin:80px auto; text-align:center">
+        <h1 style="color:#2e74b5">SWE40006 Portfolio Task 3</h1>
+        <p>Deployed by Samuel Chang via Render</p>
+        <p>Flask Web Service ✅</p>
+      </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run()
