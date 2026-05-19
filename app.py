a07b8841a6f2f375fae62c8252e0001d7411b513
Samuerson12 <113032792+Samuerson12@users.noreply.github.com>
2026-05-19 01:08:33 +0000

    Credit Task

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
       <body style="font-family:Arial; max-width:600px; margin:80px auto; text-align:center">
         <h1 style="color:#2e74b5">SWE40006 Portfolio Task 3</h1>
         <p>Deployed by Samuel Chang via Render</p>
-        <p>Flask Web Service Test ✅</p>
+        <p>Flask Web Service ✅</p>
       </body>
     </html>
     '''
